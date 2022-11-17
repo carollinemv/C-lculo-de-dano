492190f917cae9041d876d659d1eef11915e62b5
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-11-17 15:52:43 -0300

    Add files via upload

---
+++ Cálculo de dano/dano.js
@@ -0,0 +1,13 @@
+const atacante = prompt("Qual é o nome do personagem atacante?");
+const poderDeAtaque = prompt("Qual é o seu poder de ataque?");
+
+const defensor = prompt("Qual é o nome do personagem defensor?");
+let pontosDeVida = prompt("Quantos pontos de vida ele possui?");
+const poderDeDefesa = prompt("Qual é o seu poder de defesa?");
+const possuiEscudo = prompt("Ele possui um escudo?");
+
+let danoCausado = 0 
+
+if(poderDeAtaque > poderDeDefesa && possuiEscudo === "Não") {
+    
+}
