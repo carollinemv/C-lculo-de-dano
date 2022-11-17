37491067579edfe9ac8671acf0cfc9c650148b6b
carollinemv <106552762+carollinemv@users.noreply.github.com>
2022-11-17 16:01:13 -0300

    Add files via upload

--- Cálculo de dano/dano.js
+++ Cálculo de dano/dano.js
@@ -1,13 +1,15 @@
-const atacante = prompt("Qual é o nome do personagem atacante?");
-const poderDeAtaque = prompt("Qual é o seu poder de ataque?");
+const atacante = prompt("Qual é o nome do personagem atacante?")
+const poderDeAtaque = prompt("Qual é o seu poder de ataque?")
 
-const defensor = prompt("Qual é o nome do personagem defensor?");
-let pontosDeVida = prompt("Quantos pontos de vida ele possui?");
-const poderDeDefesa = prompt("Qual é o seu poder de defesa?");
-const possuiEscudo = prompt("Ele possui um escudo?");
+const defensor = prompt("Qual é o nome do personagem defensor?")
+let pontosDeVida = prompt("Quantos pontos de vida ele possui?")
+const poderDeDefesa = prompt("Qual é o seu poder de defesa?")
+const possuiEscudo = prompt("Ele possui um escudo?")
 
 let danoCausado = 0 
 
 if(poderDeAtaque > poderDeDefesa && possuiEscudo === "Não") {
-    
+    danoCausado = poderDeAtaque - poderDeDefesa
+} else if (poderDeAtaque > poderDeDefesa && possuiEscudo === "Sim") {
+    danoCausado = (poderDeAtaque - poderDeDefesa) / 2
 }
